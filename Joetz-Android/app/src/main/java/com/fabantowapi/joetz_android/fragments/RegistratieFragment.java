774062daf9bfaf4e9929891aa657ac7162022a79
Anton Rooseleer <anton.rooseleer.u9007@student.hogent.be>
2016-07-29 13:51:02 +0200

    Registreer Fragment

--- Joetz-Android/app/src/main/java/com/fabantowapi/joetz_android/fragments/RegistratieFragment.java
+++ Joetz-Android/app/src/main/java/com/fabantowapi/joetz_android/fragments/RegistratieFragment.java
@@ -5,21 +5,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.fabantowapi.joetz_android.R;
 
+import org.w3c.dom.Text;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by a_176_000 on 28-7-2016.
  */
 public class RegistratieFragment extends Fragment{
 
+    @Bind(R.id.txtEmailRegistreer)
+    private EditText txtEmail;
+    @Bind(R.id.txtWachtwoordRegistreer)
+    private EditText txtWachtwoord;
+    @Bind(R.id.txtNaam)
+    private EditText txtNaam;
+    @Bind(R.id.txtVoornaam)
+    private EditText txtVoornaam;
+    @Bind(R.id.txtRijksregisternummer)
+    private EditText txtRijksregisternumer;
+    @Bind(R.id.txtGeboorteDatum)
+    private EditText txtGeboorteDatum;
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_registreer, container, false);
         ButterKnife.bind(this, view);
+        txtEmail.requestFocus();
         return view;
+    }
+
+    @OnClick(R.id.btnRegistreer)
+    public void Registreer(){
+    // registreer logic
     }
 
 }
